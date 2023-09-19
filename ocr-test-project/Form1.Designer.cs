872cd57f3e11e9047f890f1795e0e847b6eb5434
Ahmet <Ahmet@IAMAHMETKOCA>
2023-09-19 06:15:34 +0300

    Add project files.

---
+++ ocr-test-project/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ocr_test_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rtxtResult = new System.Windows.Forms.RichTextBox();
+            this.btnChooseImage = new System.Windows.Forms.Button();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.openImage = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(419, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 339);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rtxtResult
+            // 
+            this.rtxtResult.Location = new System.Drawing.Point(12, 33);
+            this.rtxtResult.Name = "rtxtResult";
+            this.rtxtResult.Size = new System.Drawing.Size(388, 292);
+            this.rtxtResult.TabIndex = 1;
+            this.rtxtResult.Text = "";
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Location = new System.Drawing.Point(12, 340);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(115, 32);
+            this.btnChooseImage.TabIndex = 2;
+            this.btnChooseImage.Text = "Choose Image";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
+            this.btnChooseImage.Click += new System.EventHandler(this.btnChooseImage_Click);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(285, 340);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(115, 32);
+            this.btnInject.TabIndex = 3;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Image = ((System.Drawing.Image)(resources.GetObject("btnLeft.Image")));
+            this.btnLeft.Location = new System.Drawing.Point(167, 340);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(37, 32);
+            this.btnLeft.TabIndex = 4;
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Image = ((System.Drawing.Image)(resources.GetObject("btnRight.Image")));
+            this.btnRight.Location = new System.Drawing.Point(210, 339);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(37, 33);
+            this.btnRight.TabIndex = 5;
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // openImage
+            // 
+            this.openImage.FileName = "openImage";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Listen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 396);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnInject);
+            this.Controls.Add(this.btnChooseImage);
+            this.Controls.Add(this.rtxtResult);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rtxtResult;
+        private System.Windows.Forms.Button btnChooseImage;
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.OpenFileDialog openImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ocr-test-project/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+using System.Speech.Synthesis;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ocr_test_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openImage.Filter = "PNG files (*.png)|*.png|JPEG files (*.jpeg;*.jpg)|*.jpeg;*.jpg|All files (*.*)|*.*";
+
+                if (openImage.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.ImageLocation = openImage.FileName;
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error"  +exception.Message);
+            }
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(pictureBox1.Image != null)
+                {
+                    var Ocr = new IronTesseract();
+                    var result = Ocr.Read(openImage.FileName);
+                    rtxtResult.Text = result.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Not selected image");
+
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error" + exception.Message);
+            }
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipXY);
+                pictureBox1.Invalidate();
+            }
+
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Image img = pictureBox1.Image;
+                img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = img;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( rtxtResult.Text!= null )
+            {
+                Clipboard.SetText(rtxtResult.Text);
+            }
+
+        }
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textToSpeak = rtxtResult.Text;
+
+            if (rtxtResult.Text != null)
+            {
+                synth.SpeakAsync(textToSpeak);
+            }
+        }
+    }
+}
